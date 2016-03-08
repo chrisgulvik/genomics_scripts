cb8605081af56cc1840054ba27cbe1949f252f6c
Christopher Gulvik <cgulvik@gmail.com>
2016-03-08 14:46:06 -0500

    removed unnecessary vars and now cleanup automatically (discard gunzip output if GZ files given)

--- QualAssessCleanSeqs.bash
+++ QualAssessCleanSeqs.bash
@@ -33,33 +33,25 @@ fi
 #File input sequence information
 #  file regex from Illumina 1.8+ (gunzip extracted):
 #  ([0-9A-Za-z]{1,19})_S([0-9A-Za-z]{1,3})_L([0-9]{3})_R[12]_([0-9]{3}).fastq
-SAMPLE=$(basename $1 | cut -d . -f 1 | cut -d _ -f 1,2)
-SAMPLEL=$(basename $1)
-SAMPLER=$(basename $2)
-SAMPLES=$(basename $3)
+SAMPLE=$(basename $1 | cut -d . -f 1 | cut -d _ -f 1)
 
-
-###Filename handling
-dIFS=$IFS  #default IFS
-IFS=$''  #change for newlines
-#takes Trimmed unpaired R1 and R2 fq files as input
-#tests file input extensions
+#Filename handling
 if [[ $1 == *.gz && $2 == *.gz && $3 == *.gz ]]; then  #all files are gunzipped
-	SAMPLEL=$(basename $SEQ1 .gz)
-	SAMPLER=$(basename $SEQ2 .gz)
-	SAMPLES=$(basename $SEQ2 .gz)
+	SAMPLEL=$(basename $1 .gz)
+	SAMPLER=$(basename $2 .gz)
+	SAMPLES=$(basename $3 .gz)
 	echo "extracting the gunzipped file $1..."
-	gunzip -c $1 > "$4"/"$SAMPLEL"
+	gunzip -c "$1" > "$4"/"$SAMPLEL"
 	echo "$1 was extracted..."
-	SEQ1=$("$4"/"$SAMPLEL")
+	SEQ1="$4"/"$SAMPLEL"
 	echo "extracting the gunzipped file $2..."
-	gunzip -c $2 > "$4"/"$SAMPLER"
+	gunzip -c "$2" > "$4"/"$SAMPLER"
 	echo "$2 was extracted..."
-	SEQ2=$("$4"/"$SAMPLER")
+	SEQ2="$4"/"$SAMPLER"
 	echo "extracting the gunzipped file $3..."
-	gunzip -c $3 > "$4"/"$SAMPLES"
+	gunzip -c "$3" > "$4"/"$SAMPLES"
 	echo "$3 was extracted..."
-	SEQ3=$("$4"/"$SAMPLES")
+	SEQ3="$4"/"$SAMPLES"
 elif [[ $1 == *q && $2 == *q && $3 == *q ]]; then  #all files are fq format
 	echo "all three files are in FastQ format..."
 	SEQ1="$1"
@@ -69,14 +61,13 @@ else  #for now skip possibily of some but not all gunzipped
 	usage
 	exit 1
 fi
-IFS=$dIFS  #back to default IFS
 wait
 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo " $SAMPLEL, $SAMPLER,"
-echo " and $SAMPLES sequences were read in..."
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo " $SEQ1, $SEQ2,"
+echo " and $SEQ3 sequences were read in..."
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Count total nucleotides
 TN1=$(awk 'BEGIN {SUM=0;} {if(NR%4==2) {SUM+=length($0);}} END {print SUM;}' $SEQ1)
@@ -84,13 +75,13 @@ TN2=$(awk 'BEGIN {SUM=0;} {if(NR%4==2) {SUM+=length($0);}} END {print SUM;}' $SE
 TN3=$(awk 'BEGIN {SUM=0;} {if(NR%4==2) {SUM+=length($0);}} END {print SUM;}' $SEQ3)
 BP_TOT="$(($TN1 + $TN2 + $TN3))"
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo " counted the total nucleotides..."
 echo "        Total:  $BP_TOT"
 echo "        R1:     $TN1"
 echo "        R2:     $TN2"
 echo "        single: $TN3"
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 OPSYS=$(uname -s)
 
@@ -123,15 +114,15 @@ wait
 Q20_TOT="$(($Q201 + $Q202 + $Q203))" 
 Q30_TOT="$(($Q301 + $Q302 + $Q303))" 
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' counted the Q20+ and Q30+ nucleotides...'
-echo "        ${Q201} nucleotides are >=Q20 in $SAMPLEL" 
-echo "        ${Q301} nucleotides are >=Q30 in $SAMPLEL" 
-echo "        ${Q202} nucleotides are >=Q20 in $SAMPLER" 
-echo "        ${Q302} nucleotides are >=Q30 in $SAMPLER" 
-echo "        ${Q203} nucleotides are >=Q20 in $SAMPLES" 
-echo "        ${Q303} nucleotides are >=Q30 in $SAMPLES" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo "        ${Q201} nucleotides are >=Q20 in $SEQ1" 
+echo "        ${Q301} nucleotides are >=Q30 in $SEQ1" 
+echo "        ${Q202} nucleotides are >=Q20 in $SEQ2" 
+echo "        ${Q302} nucleotides are >=Q30 in $SEQ2" 
+echo "        ${Q203} nucleotides are >=Q20 in $SEQ3" 
+echo "        ${Q303} nucleotides are >=Q30 in $SEQ3" 
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Calculate percentages of >=Q20 and >=Q30
 PQ201="$(echo "scale=2;($Q201/$TN1)*100" | bc)"
@@ -141,15 +132,15 @@ PQ301="$(echo "scale=2;($Q301/$TN1)*100" | bc)"
 PQ302="$(echo "scale=2;($Q302/$TN2)*100" | bc)"
 PQ303="$(echo "scale=2;($Q303/$TN3)*100" | bc)"
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' calculated the percentages of Q20 and Q30...'
-echo "        ${PQ201}% nucleotides are >=Q20 in $SAMPLEL" 
-echo "        ${PQ301}% nucleotides are >=Q30 in $SAMPLEL" 
-echo "        ${PQ202}% nucleotides are >=Q20 in $SAMPLER" 
-echo "        ${PQ302}% nucleotides are >=Q30 in $SAMPLER" 
-echo "        ${PQ203}% nucleotides are >=Q20 in $SAMPLES" 
-echo "        ${PQ303}% nucleotides are >=Q30 in $SAMPLES" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo "        ${PQ201}% nucleotides are >=Q20 in $SEQ1" 
+echo "        ${PQ301}% nucleotides are >=Q30 in $SEQ1" 
+echo "        ${PQ202}% nucleotides are >=Q20 in $SEQ2" 
+echo "        ${PQ302}% nucleotides are >=Q30 in $SEQ2" 
+echo "        ${PQ203}% nucleotides are >=Q20 in $SEQ3" 
+echo "        ${PQ303}% nucleotides are >=Q30 in $SEQ3" 
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Count number of reads
 NUMLN_L=$(wc -l $SEQ1| awk '{print $1}')
@@ -161,21 +152,22 @@ NUM_R="$(($NUMLN_R / 4))"
 NUM_single="$(($NUMLN_single / 4))"
 READ_TOT="$(($NUM_L + NUM_R + NUM_single))"
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' counted the number of reads...'
 echo "        There are $NUM_L reads in $SAMPLEL" 
 echo "        There are $NUM_R reads in $SAMPLER" 
 echo "        There are $NUM_single reads in $SAMPLES" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Create results output file
 printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' \
 Sample_Name Q20_Total_[bp] Q30_Total_[bp] Q20_R1_[bp] Q20_R2_[bp] Q20_single_[bp] Q20_R1_[%] Q20_R2_[%] Q20_single_[%] Q30_R1_[bp] Q30_R2_[bp] Q30_single_[bp] Q30_R1_[%] Q30_R2_[%] Q30_single_[%] Total_Sequenced_[bp] Total_Sequenced_[reads] NUMR1_[bp] NUMR2_[bp] NUMsingle_[bp] \
-"$SAMPLE" "$Q20_TOT" "$Q30_TOT" "$Q201" "$Q202" "$Q203" "$PQ201%" "$PQ202"% "$PQ203"% "$Q301" "$Q302" "$Q303" "$PQ301"% "$PQ302"% "$PQ303"% "$BP_TOT" "$READ_TOT" "$NUM_L" "$NUM_R" "$NUM_single" > "$4"/QualAssessTrimmedSeqs_"$SAMPLE"_results.tsv ;
+"$SAMPLE" "$Q20_TOT" "$Q30_TOT" "$Q201" "$Q202" "$Q203" "$PQ201%" "$PQ202%" "$PQ203%" "$Q301" "$Q302" "$Q303" "$PQ301%" "$PQ302%" "$PQ303%" "$BP_TOT" "$READ_TOT" "$NUM_L" "$NUM_R" "$NUM_single" 
+> "$4"/QualAssessTrimSeqs_"$SAMPLE"_results.tab ;
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' created results output file...'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 if [[ "$OPSYS" == Linux ]]; then
 	R1FILEPATH=$(readlink -m $1)
@@ -187,14 +179,18 @@ elif [[ "$OPSYS" == Darwin ]]; then
 	singleFILEPATH=$(echo `pwd`/`ls "$3"`)
 fi
 
+[[ $1 == *.gz ]] && rm -f "$3"/"$SAMPLEL" 
+[[ $2 == *.gz ]] && rm -f "$3"/"$SAMPLER"
+[[ $3 == *.gz ]] && rm -f "$3"/"$SAMPLES"
+
 #Create log file
 printf "`date`\n$USER\n%s\n%s\n\n" \
 "$R1FILEPATH" "$R2FILEPATH" "$singleFILEPATH" \
-> "$4"/QualAssessTrimmedSeqs_"$SAMPLE"_results.log ;
+> "$4"/QualAssessTrimSeqs_"$SAMPLE"_results.log ;
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' created log output file...'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo " Quality assessment of $SAMPLE Trimmed sequences completed" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo " Quality assessment of $SAMPLE trimmed sequences completed" 
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
--- QualAssessRawSeqs.bash
+++ QualAssessRawSeqs.bash
@@ -33,65 +33,60 @@ fi
 #File input sequence information
 #  file regex from Illumina 1.8+ (gunzip extracted):
 #  ([0-9A-Za-z]{1,19})_S([0-9A-Za-z]{1,3})_L([0-9]{3})_R[12]_([0-9]{3}).fastq
-SAMPLE=$(basename $1 | cut -d . -f 1 | cut -d _ -f 1,2)
-SAMPLEL=$(basename $1)
-SAMPLER=$(basename $2)
+SAMPLE=$(basename $1 | cut -d . -f 1 | cut -d _ -f 1)
 
 #Filename handling
-dIFS=$IFS  #default IFS
-IFS=$''  #change for newlines
 if [[ $1 == *.gz && $2 == *.gz ]]; then  #both files are gunzipped
-	SAMPLEL=$(basename $SEQ1 .gz)
-	SAMPLER=$(basename $SEQ2 .gz)
+	SAMPLEL=$(basename $1 .gz)
+	SAMPLER=$(basename $2 .gz)
 	echo "extracting the gunzipped file $1..."
-	gunzip -c $1 > "$3"/"$SAMPLEL"
+	gunzip -c "$1" > "$3"/"$SAMPLEL"
 	echo "$1 was extracted..."
-	SEQ1=$("$3"/"$SAMPLEL")
+	SEQ1="$3"/"$SAMPLEL"
 	echo "extracting the gunzipped file $2..."
-	gunzip -c $2 > "$3"/"$SAMPLER"
+	gunzip -c "$2" > "$3"/"$SAMPLER"
 	echo "$2 was extracted..."
-	SEQ2=$("$3"/"$SAMPLER")
+	SEQ2="$3"/"$SAMPLER"
 elif [[ $1 == *q && $2 == *q ]]; then  #both files are fastq format
 	echo "both are FastQ format..."
-	SEQ1=$(echo $1)
-	SEQ2=$(echo $2)
+	SEQ1="$1"
+	SEQ2="$2"
 elif [[ $1 == *.gz && $2 == *q ]]; then
-	SAMPLEL=$(basename $SEQ1 .gz)
+	SAMPLEL=$(basename $1 .gz)
 	echo "extracting the gunzipped file $1..."
-	gunzip -c $1 > "$3"/"$SAMPLEL"
+	gunzip -c "$1" > "$3"/"$SAMPLEL"
 	echo "$1 was extracted..."
-	SEQ1=$("$3"/"$SAMPLEL")
+	SEQ1="$3"/"$SAMPLEL"
 	SEQ2="$2"  #$2 is still in fastq format
 elif [[ $1 == *q && $2 == *.gz ]]; then
-	SAMPLER=$(basename $SEQ2 .gz)
+	SAMPLER=$(basename $2 .gz)
 	SEQ1="$1"  #$1 is still in fastq format
 	echo "extracting the gunzipped file $2..."
-	gunzip -c $2 > "$3"/"$SAMPLER"
+	gunzip -c "$2" > "$3"/"$SAMPLER"
 	echo "$2 was extracted..."
-	SEQ2=$("$3"/"$SAMPLER")
+	SEQ2="$3"/"$SAMPLER"
 else
 	usage
 	exit 1
 fi
-IFS=$dIFS  #back to default IFS
 wait
 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo " $SAMPLEL and $SAMPLER sequences were read in..."
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo " $SEQ1 and $SEQ2 sequences were read in..."
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Count total nucleotides
 TN1=$(awk 'BEGIN {SUM=0;} {if(NR%4==2) {SUM+=length($0);}} END {print SUM;}' $SEQ1)
 TN2=$(awk 'BEGIN {SUM=0;} {if(NR%4==2) {SUM+=length($0);}} END {print SUM;}' $SEQ2)
 BP_TOT="$(($TN1 + $TN2))"
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo " counted the total nucleotides..."
 echo "        Total: $BP_TOT"
 echo "        R1:    $TN1"
 echo "        R2:    $TN2"
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 OPSYS=$(uname -s)
 
@@ -118,13 +113,13 @@ wait
 Q20_TOT="$(($Q20L + $Q20R))" 
 Q30_TOT="$(($Q30L + $Q30R))" 
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' counted the Q20+ and Q30+ nucleotides...'
-echo "        ${Q20L} nucleotides are >=Q20 in $SAMPLEL" 
-echo "        ${Q30L} nucleotides are >=Q30 in $SAMPLEL" 
-echo "        ${Q20R} nucleotides are >=Q20 in $SAMPLER" 
-echo "        ${Q30R} nucleotides are >=Q30 in $SAMPLER" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo "        ${Q20L} nucleotides are >=Q20 in $SEQ1" 
+echo "        ${Q30L} nucleotides are >=Q30 in $SEQ1" 
+echo "        ${Q20R} nucleotides are >=Q20 in $SEQ2" 
+echo "        ${Q30R} nucleotides are >=Q30 in $SEQ2" 
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Calculate percentages of >=Q20 and >=Q30
 PQ20L="$(echo "scale=2;($Q20L/$TN1)*100" | bc)"
@@ -132,13 +127,13 @@ PQ20R="$(echo "scale=2;($Q20R/$TN2)*100" | bc)"
 PQ30L="$(echo "scale=2;($Q30L/$TN1)*100" | bc)"
 PQ30R="$(echo "scale=2;($Q30R/$TN2)*100" | bc)"
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' calculated the percentages of Q20+ and Q30+...'
-echo "        ${PQ20L}% nucleotides are >=Q20 in $SAMPLEL" 
-echo "        ${PQ30L}% nucleotides are >=Q30 in $SAMPLEL" 
-echo "        ${PQ20R}% nucleotides are >=Q20 in $SAMPLER" 
-echo "        ${PQ30R}% nucleotides are >=Q30 in $SAMPLER" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo "        ${PQ20L}% nucleotides are >=Q20 in $SEQ1" 
+echo "        ${PQ30L}% nucleotides are >=Q30 in $SEQ1" 
+echo "        ${PQ20R}% nucleotides are >=Q20 in $SEQ2" 
+echo "        ${PQ30R}% nucleotides are >=Q30 in $SEQ2" 
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Count number of reads
 NUMLN_L=$(wc -l $SEQ1| awk '{print $1}')
@@ -148,20 +143,21 @@ NUM_L="$(($NUMLN_L / 4))"
 NUM_R="$(($NUMLN_R / 4))"
 READ_TOT="$(($NUM_L + NUM_R))"
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' counted the number of reads...'
-echo "        There are $NUM_L reads in $SAMPLEL" 
-echo "        There are $NUM_R reads in $SAMPLER" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo "        There are $NUM_L reads in $SEQ1" 
+echo "        There are $NUM_R reads in $SEQ2" 
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 #Create results output file
 printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' \
 Sample_Name Q20_Total_[bp] Q30_Total_[bp] Q20_R1_[bp] Q20_R2_[bp] Q20_R1_[%] Q20_R2_[%] Q30_R1_[bp] Q30_R2_[bp] Q30_R1_[%] Q30_R2_[%] Total_Sequenced_[bp] Total_Sequenced_[reads] NUMR1_[bp] NUMR2_[bp] \
-"$SAMPLE" "$Q20_TOT" "$Q30_TOT" "$Q20L" "$Q20R" "$PQ20L%" "$PQ20R%" "$Q30L" "$Q30R" "$PQ30L%" "$PQ30R%" "$BP_TOT" "$READ_TOT" "$NUM_L" "$NUM_R" > "$3"/QualAssessRawSeqs_"$SAMPLE"_results.tsv ;
+"$SAMPLE" "$Q20_TOT" "$Q30_TOT" "$Q20L" "$Q20R" "$PQ20L%" "$PQ20R%" "$Q30L" "$Q30R" "$PQ30L%" "$PQ30R%" "$BP_TOT" "$READ_TOT" "$NUM_L" "$NUM_R" \
+> "$3"/QualAssessRawSeqs_"$SAMPLE"_results.tab ;
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' created results output file...'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 if [[ "$OPSYS" == Linux ]]; then
 	R1FILEPATH=$(readlink -m $1)
@@ -171,14 +167,17 @@ elif [[ "$OPSYS" == Darwin ]]; then
 	R2FILEPATH=$(echo `pwd`/`ls "$2"`)
 fi
 
+[[ $1 == *.gz ]] && rm -f "$3"/"$SAMPLEL" 
+[[ $2 == *.gz ]] && rm -f "$3"/"$SAMPLER"
+
 #Create log file
 printf "`date`\n$USER\n%s\n%s\n\n" \
 "$R1FILEPATH" "$R2FILEPATH" \
 > "$3"/QualAssessRawSeqs_"$SAMPLE"_results.log ;
 wait
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo ' created log output file...'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 echo " Quality assessment of $SAMPLE raw sequences completed" 
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
