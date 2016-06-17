d186d5263b588a9d5a13b9a52fe385d7307b554c
Chris Gulvik <cgulvik@gmail.com>
2016-06-17 16:43:26 -0400

    when ambiguous amino acid found, estimate MW and indicate with ~ as prefix to MW

--- gbk2proteome-molec-weights.py
+++ gbk2proteome-molec-weights.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 
 # Given a genbank file, prints to stdout a tab-delimited summary
-# of the proteome including the molecular weight in Daltons
+# of the proteome including the molecular weight in Daltons, where
+# '~<value>' indicates the MW is estimated due to ambiguous amino acid(s)
+
 # Usage: python script.py <input.gbk>
 
 from Bio import SeqIO
 from Bio import SeqUtils
-from decimal import Decimal, ROUND_DOWN, ROUND_UP
+from decimal import Decimal, ROUND_UP
 import os
+import string
 import sys
 
-infile = SeqIO.parse(sys.argv[1], 'genbank')
+def calc_molec_weight(protein_seq):
+	return Decimal(SeqUtils.molecular_weight(protein_seq, seq_type='protein')
+		).quantize(Decimal('.001'), rounding=ROUND_UP)
+
+infile   = SeqIO.parse(sys.argv[1], 'genbank')
 filename = os.path.splitext(os.path.splitext(os.path.basename(sys.argv[1]))[0])[0]
 
 for rec in infile:
 	for s in rec.features:
 		if (s.type == 'CDS' and 'gene') in s.qualifiers:
-			gene       = s.qualifiers['gene'][0]
-			locus      = s.qualifiers['locus_tag'][0]
-			product    = s.qualifiers['product'][0]
-			protein_mw = Decimal(SeqUtils.molecular_weight(s.qualifiers['translation'][0],
-							seq_type='protein')).quantize(Decimal('.001'), rounding=ROUND_UP)
+			gene    = s.qualifiers['gene'][0]
+			locus   = s.qualifiers['locus_tag'][0]
+			product = s.qualifiers['product'][0]
+			protein = s.qualifiers['translation'][0]
+			if 'X' in protein:
+				num_X = protein.count('X')
+				estimated_mw = Decimal(num_X) * Decimal('128.16')  #UNK=(C5)(H6)(N1)(O3)=128.16
+				peptide_mw   = calc_molec_weight(protein.replace('X', ''))
+				protein_mw   = '~' + str(peptide_mw + estimated_mw)
+			else:
+				protein_mw   = calc_molec_weight(protein)
 			print '{}\t{}\t{}\t{}'.format(gene,locus,product,protein_mw)
