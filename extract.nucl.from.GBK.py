141fd4acbecae45fa91f1aba8c08f6dac59fe944
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2017-06-22 15:24:02 -0400

    allow >1 record match and print to stdout by default

--- extract.nucl.from.GBK.py
+++ extract.nucl.from.GBK.py
@@ -13,7 +13,8 @@ from Bio.SeqRecord import SeqRecord
 
 def parseArgs():
 	parser = ArgumentParser(description='Extracts a nucleotide sequence from '
-		'a GenBank file', add_help=False)
+		'a GenBank file', epilog='NOTE: if >1 record is found, a warning is '
+		'printed to stderr, and all matches are sent as output', add_help=False)
 	req = parser.add_argument_group('Required')
 	req.add_argument('-i', '--infile', required=True, metavar='FILE',
 		help='input GenBank file with translations')
@@ -25,8 +26,8 @@ def parseArgs():
 	opt.add_argument('-t', '--type', default='locus_tag', metavar='STR',
 		help='genbank feature type to search in, e.g., CDS, gene, rRNA, '
 		'source, misc_feature')
-	opt.add_argument('-o', '--outfile', default='parsed.fas', metavar='FILE',
-		help='output FastA file  [./parsed.fas]')
+	opt.add_argument('-o', '--outfile', required=False, default=None,
+		help='FastA output [stdout]')
 	return parser.parse_args()
 
 def index_genbank(rec, feature_type, qualifier) :
@@ -42,7 +43,7 @@ def index_genbank(rec, feature_type, qualifier) :
 	return gb_idx
 
 def locus_tag2gene_seq(gbk, ft_type, query, outfile):
-	r = 0
+	records = []
 	for rec in SeqIO.parse(open(gbk, 'r'), 'genbank'):
 		gbk_idx = index_genbank(rec, 'CDS', ft_type)
 		if query in gbk_idx:
@@ -50,13 +51,19 @@ def locus_tag2gene_seq(gbk, ft_type, query, outfile):
 			product = str(rec.features[gbk_idx[query]].qualifiers['product'][0])
 			record = SeqRecord(Seq(gene_seq, generic_dna), id=query,
 				description=product, name='')
-			r += 1
-	if r == 1:
-		SeqIO.write(record, outfile, 'fasta')
-	elif r > 1:
-		print 'ERROR: found >1 {}'.format(query)
+			records.append(record)
+
+	if len(records) == 0:
+		sys.stderr.write('ERROR: {} absent\n'.format(query))
+		sys.exit(1)
+	elif len(records) > 1:
+		sys.stderr.write('WARNING: found >1 {}\n'.format(query))
+	
+	if outfile:
+		outfile = os.path.abspath(os.path.expanduser(outfile))
+		SeqIO.write(records, outfile, 'fasta')
 	else:
-		print 'ERROR: {} absent'.format(query)
+		SeqIO.write(records, sys.stdout, 'fasta')
 
 def main():
 	opts = parseArgs()
