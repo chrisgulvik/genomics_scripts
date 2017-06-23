93035c8c970d05b908908e1e269c2e9306640e59
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2017-06-23 12:47:16 -0400

    added opts to enable prefix, suffix, and extension naming for each split file

--- split.multifasta.py
+++ split.multifasta.py
@@ -1,42 +1,71 @@
 #!/usr/bin/env python
 
-import argparse
+
 import os
 import sys
+from argparse import ArgumentParser
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 def parseArgs():
-	parser = argparse.ArgumentParser(
-		description='splits multi-FastA file into individual FastA files')
-	parser.add_argument('-i', '--infile',
-		required=True, help='input multi-FastA file')
-	parser.add_argument('-o', '--outdir',
-		help='output directory path [cwd of input file]')
-	parser.add_argument('-g', '--nogaps', action='store_true', default=False,
-		help='toggle on removal of \'~\' and \'-\' gaps [off]')
+	parser = ArgumentParser(add_help=False,
+		description='splits multi-FastA file into individual FastA files',
+		epilog='NOTE: for each split file, the output defline will be the '
+		'filename excluding the extension')
+	req = parser.add_argument_group('Required')
+	req.add_argument('-i', '--infile', required=True, metavar='FILE',
+		help='input multi-FastA file')
+	opt = parser.add_argument_group('Optional')
+	opt.add_argument('-e', '--ext', metavar='STR', type=str, default='.fasta',
+		help='extension name for each output file [\'.fasta\']')
+	opt.add_argument('-g', '--nogaps', action='store_true', default=False,
+		help='toggle on removal of \'-\' gaps [off]')
+	opt.add_argument('-h', '--help', action='help',
+		help='show this help message and exit')
+	opt.add_argument('-o', '--outdir', metavar='PATH',
+		help='output directory path [dir of input file]')
+	opt.add_argument('-p', '--prefix', metavar='STR', type=str, default=None,
+		help='prefix name for each output file [defline from input file; '
+		'first string on whitespace]')
+	opt.add_argument('-s', '--suffix', metavar='STR', type=str, default=None,
+		help='suffix name for each output file [\'_<int++>\']')
 	return parser.parse_args()
 
 def main():
-	args = parseArgs()
-	infile = args.infile
-	outdir = args.outdir
+	opt = parseArgs()
 
-	if outdir is None:
+	# I/O handling
+	infile = os.path.abspath(os.path.expanduser(opt.infile))
+	if opt.outdir is None:
 		outdir = os.path.dirname(infile)
-
+	else:
+		outdir = opt.os.path.abspath(os.path.expanduser(opt.outdir))
 	mfasta = SeqIO.parse(infile, 'fasta')
 
+	i = 1
 	for rec in mfasta:
+		# Sequence handling
 		seq  = str(rec.seq).upper()
-		if args.nogaps:
-			seq = seq.replace('-', '').replace('~', '')
-		name = (rec.id).split()[0]
-		new_rec = SeqRecord(Seq(seq, generic_dna), id=name, description='')
-		SeqIO.write(new_rec, '{}.fasta'.format(name), 'fasta')
+		if opt.nogaps:
+			seq = seq.replace('-', '')
 
+		# Defline and filename handling
+		if opt.prefix is not None:
+			pref = opt.prefix
+		else:
+			pref = (rec.id).split()[0]
+		if opt.suffix is not None:
+			suff = opt.suffix
+		else:
+			suff = '_{}'.format(i)
+			i += 1
+
+		# Output each record as separate file
+		new_rec = SeqRecord(Seq(seq, generic_dna), id=pref + suff,
+			description='')
+		SeqIO.write(new_rec, pref + suff + opt.ext, 'fasta')
 
 if __name__ == '__main__':
 	main()
