74b2ba50d3ac2461023d32b3586a93db4fa818b8
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2018-10-29 17:14:58 -0400

    added opt to label unambiguous regions; renamed ambiguous naming opt

--- fasta2genbank.py
+++ fasta2genbank.py
@@ -2,10 +2,10 @@
 
 
 import os
+import re
 import sys
 from argparse import ArgumentParser
 from itertools import groupby
-from operator import itemgetter
 from shutil import rmtree
 from tempfile import mkdtemp
 from Bio import SeqIO, Alphabet
@@ -22,9 +22,11 @@ def parseArgs():
 		help='show this help message and exit')
 	opt.add_argument('-o', '--output', metavar='FILE', default=None,
 		help='output GenBank file [stdout]')
-	opt.add_argument('--label-ambiguous-features', action='store_true',
+	opt.add_argument('--label-ambiguous', action='store_true',
 		default=False, help='label positions where N occurs as \'ambiguous\' '
 		'[off]')
+	opt.add_argument('--label-unambiguous', action='store_true',
+		default=False, help='label positions where ATCG occur [off]')
 	return parser.parse_args()
 
 def collapse_integers_to_ranges(sites):
@@ -38,6 +40,41 @@ def collapse_integers_to_ranges(sites):
 			ranges.append([group[0], group[0]])
 	return ranges
 
+def label_sites_to_gbkrec(rec, sites, label):
+	ranges = collapse_integers_to_ranges(sites)
+	for begin_end in ranges:
+		sf = SeqFeature(FeatureLocation(begin_end[0], begin_end[1]+1),
+			type=label, id=label, qualifiers={label:'yes',
+			'note':'{} nucleotide region'.format(label)})
+		rec.features.append(sf)
+	return rec
+
+def label_ambiguous_gbkrecs(gbkrecs):
+	records = []
+	for rec in gbkrecs:
+		# Find ambiguous N nucleotides
+		i, split_len = 0, 0
+		sites = []
+		seq = rec.seq.upper()
+		while i >= 0:
+			i = seq.find('N')
+			if i >= 0:
+				sites.append(i + split_len)
+				split_len += i+1
+				seq = seq[i+1:]
+		records.append(label_sites_to_gbkrec(rec, sites, 'ambiguous'))
+	return records
+
+def label_unambiguous_gbkrecs(gbkrecs):
+	records = []
+	for rec in gbkrecs:
+		# Find unambiguous ATCG nucleotides
+		seq = str(rec.seq.upper())
+		sites = [m.start() for m in re.finditer(
+			'|'.join(map(re.escape, 'ATCG')), seq)]
+		records.append(label_sites_to_gbkrec(rec, sites, 'unambiguous'))
+	return records
+
 def main():
 	opt = parseArgs()
 	ifh = os.path.abspath(os.path.expanduser(opt.input))
@@ -46,7 +83,7 @@ def main():
 	else:
 		ofh = sys.stdout
 
-	if not opt.label_ambiguous_features:
+	if not opt.label_ambiguous and not opt.label_unambiguous:
 		SeqIO.convert(ifh, 'fasta', ofh, 'genbank',
 			alphabet=Alphabet.generic_dna)
 		sys.exit(0)
@@ -55,32 +92,16 @@ def main():
 	temp_gbk = os.path.join(tmp, 'gb')
 	SeqIO.convert(ifh, 'fasta', temp_gbk, 'genbank',
 		alphabet=Alphabet.generic_dna)
+	with open(temp_gbk) as tfh:
+		gbkrecs = list(SeqIO.parse(tfh, 'genbank'))
+	if opt.label_ambiguous:
+		gbkrecs = label_ambiguous_gbkrecs(gbkrecs)
+	if opt.label_unambiguous:
+		gbkrecs = label_unambiguous_gbkrecs(gbkrecs)
 
-	records = []
-	with open(temp_gbk) as i:
-		for rec in SeqIO.parse(i, 'genbank'):
-			# Find ambiguous N nucleotides
-			i, split_len = 0, 0
-			sites = []
-			seq = rec.seq.upper()
-			while i >= 0:
-				i = seq.find('N')
-				if i >= 0:
-					sites.append(i + split_len)
-					split_len += i+1
-					seq = seq[i+1:]
-
-			# Label ambiguous sites in GenBank file
-			ranges = collapse_integers_to_ranges(sites)
-			for begin_end in ranges:
-				sf = SeqFeature(FeatureLocation(begin_end[0], begin_end[1]+1),
-					type='ambiguous')
-				rec.features.append(sf)
-			records.append(rec)
-
-	SeqIO.write(records, ofh, 'genbank')
+	SeqIO.write(gbkrecs, ofh, 'genbank')
 	sys.stderr.write('INFO: {} sequence records written\n'.format(
-		len(records)))
+		len(gbkrecs)))
 	rmtree(tmp)
 
 if __name__ == '__main__':
