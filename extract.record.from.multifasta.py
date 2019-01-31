f74517399d39945a37fdf23e0b5eaaecbb5adab7
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2019-01-31 13:43:47 -0500

    auto-detect gz compressed input; optional search type for full_exact matches

--- extract.record.from.multifasta.py
+++ extract.record.from.multifasta.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+import gzip
 import os
 import sys
 from argparse import ArgumentParser
@@ -12,7 +13,7 @@ def parseArgs():
 		' query string', add_help=False)
 	req = parser.add_argument_group('Required')
 	req.add_argument('-i', '--infile', required=True,
-		help='input multi-FastA file')
+		help='input multi-FastA file, optionally gunzip compressed')
 	req.add_argument('-q', '--query', required=True,
 		help='string to search deflines')
 	opt = parser.add_argument_group('Optional')
@@ -20,6 +21,9 @@ def parseArgs():
 		help='show this help message and exit')
 	opt.add_argument('-o', '--outfile', required=False, default=None,
 		help='FastA output [stdout]')
+	opt.add_argument('-y', '--search-type', default='contains',
+		choices=['contains', 'full_exact'], help='type of query match '
+		'[default: contains]')
 	return parser.parse_args()
 
 def main():
@@ -27,11 +31,17 @@ def main():
 	infile = os.path.abspath(os.path.expanduser(opt.infile))
 	query  = opt.query
 
+	if infile.endswith('.gz'):
+		infile = gzip.open(infile)
 	mfasta = SeqIO.parse(infile, 'fasta')
 	query_match = []
 	for record in mfasta:
-		if str(query) in record.description:
-			query_match.append(record)
+		if opt.search_type == 'contains':
+			if str(query) in record.description:
+				query_match.append(record)
+		elif opt.search_type == 'full_exact':
+			if str(query) == record.description:
+				query_match.append(record)
 
 	if len(query_match) == 0:
 		sys.stderr.write('ERROR: {} absent from deflines\n'.format(query))
