2f9bde50597321ece07a5e291080f88d28750c6d
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2020-03-12 10:59:39 -0400

    use realpath; added GC filter to avoid outputting empty contigs

--- genbank2gff.py
+++ genbank2gff.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 
+import gzip
 import os
 import sys
 from argparse import ArgumentParser
 from BCBio import GFF  #pip install bcbio-gff
 from Bio import SeqIO
+from Bio.SeqUtils import GC
 
 def parseArgs():
 	parser = ArgumentParser(description='Converts a GenBank file containing '
@@ -18,13 +20,24 @@ def parseArgs():
 
 def main():
 	opt = parseArgs()
-	ifh = os.path.abspath(os.path.expanduser(opt.infile))
+	ifh = os.path.realpath(os.path.expanduser(opt.infile))
 	if opt.outfile is not None:
-		ofh = open(os.path.abspath(os.path.expanduser(opt.outfile)), 'w')
+		ofh = os.path.realpath(os.path.expanduser(opt.outfile))
 	else:
 		ofh = sys.stdout
-	with open(ifh) as i:
-		GFF.write(SeqIO.parse(i, 'genbank'), ofh, include_fasta=True)
+
+	records = []
+	if ifh.endswith('.gz'):
+		ifh = gzip.open(ifh)
+	for rec in SeqIO.parse(ifh, 'genbank'):
+		# halt if nucleotides are absent rather than printing default 'N'
+		if float(GC(rec.seq)) == 0:
+			sys.stderr.write('ERROR: {} appears to lack nucleotides\n'.\
+				format(ifh))
+			sys.exit(1)
+		records.append(rec)
+	with open(ofh, 'w') as o:
+		GFF.write(records, o, include_fasta=True)
 
 if __name__ == '__main__':
 	main()
